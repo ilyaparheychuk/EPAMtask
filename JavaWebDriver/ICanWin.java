1cba01742ec9916ad5eb0960592188cc7c833fc7
unknown <iliyaparheychuk@gmail.com>
2019-09-25 15:46:41 +0300

    JavaWebDriver

---
+++ JavaWebDriver/ICanWin.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ICanWin {
+
+    public static void main(String[] args){
+        System.setProperty("webdriver.chrome.driver", "D:/studyautomate/chromedriver/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        Page page = new Page(driver);
+        page.iCanWin("Hello from WebDriver", "helloweb");
+        driver.quit();
+    }
+
+}
